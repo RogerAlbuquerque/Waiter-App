9115ad0a5f6f284488ccd83e10f8b6d46f2943ee
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2022-12-08 15:53:34 -0300

    categories componente ALL OK

--- app/src/components/Categories/index.tsx
+++ app/src/components/Categories/index.tsx
@@ -3,11 +3,20 @@ import { FlatList } from 'react-native';
 import {categories} from '../../mocks/categories';
 import { Category, Icon } from './styles';
 import { Image } from 'react-native';
+import { useState } from 'react';
+import { Text } from '../Text';
 
 
 export function Categories(){
-  return (
 
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleSelectCategory(categoryId: string){
+    const category = selectedCategory === categoryId ? '' : categoryId;
+    setSelectedCategory(category);
+  }
+
+  return (
 
 
     <FlatList
@@ -16,18 +25,26 @@ export function Categories(){
       data={categories}
       keyExtractor={category => category._id}
       contentContainerStyle = {{paddingRight: 24}}
-      renderItem={({item: category})=>(
-        <Category>
+      renderItem={({item: category})=>{
+        const isSelected = selectedCategory === category._id;
+        return(
 
-          <Icon>
-            {category.icon == 'pizza'      && <Image source={require('../../mocks/categoriesIcons/pizza.png')}/>}
-            {category.icon == 'bebidas'    && <Image source={require('../../mocks/categoriesIcons/bebidas.png')}/>}
-            {category.icon == 'hamburguer' && <Image source={require('../../mocks/categoriesIcons/hamburguer.png')}/>}
-            {category.icon == 'promo'      && <Image source={require('../../mocks/categoriesIcons/promo.png')}/>}
-          </Icon>
+          <Category onPress={() => handleSelectCategory(category._id)}>
 
-        </Category>
-      )}
+            <Icon style={{opacity: isSelected ? 1 : 0.5}}>
+              {category.icon == 'pizza'      && <Image source={require('../../mocks/categoriesIcons/pizza.png')}/>}
+              {category.icon == 'bebidas'    && <Image source={require('../../mocks/categoriesIcons/bebidas.png')}/>}
+              {category.icon == 'hamburguer' && <Image source={require('../../mocks/categoriesIcons/hamburguer.png')}/>}
+              {category.icon == 'promo'      && <Image source={require('../../mocks/categoriesIcons/promo.png')}/>}
+            </Icon>
+
+            <Text size={14} weight="600" opacity={isSelected ? 1 : 0.5}>{category.name}</Text>
+
+          </Category>
+
+        );
+      }
+      }
 
     />
 
--- app/src/components/Categories/styles.ts
+++ app/src/components/Categories/styles.ts
@@ -3,7 +3,7 @@ import { Platform } from 'react-native';
 
 const isAndroid = Platform.OS ==='android';
 
-export const Category = styled.View`
+export const Category = styled.TouchableOpacity`
 justify-content: center;
 align-items: center;
 margin: 24px;
