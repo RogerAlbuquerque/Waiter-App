171e0acce54169f7635fcc02f395f18189d74e0b
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2022-12-11 13:39:40 -0300

    reoganized identation again names on header componente on header

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -6,8 +6,7 @@ export function Header(){
     <Container>
       <Text size={14} opacity={0.9}>Bem-vindo(a) ao</Text>
       <Text size={24} weight="700">
-        WAITTER
-        <Text size={24}>APP</Text>
+        WAITTER<Text size={24}>APP</Text>
       </Text>
     </Container>
   );
