0d35a244c8d9837080ce2d6ecb48d61f82033ef8
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2022-12-11 13:38:38 -0300

    reorganized names on header componente on app

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -7,10 +7,7 @@ export function Header(){
       <Text size={14} opacity={0.9}>Bem-vindo(a) ao</Text>
       <Text size={24} weight="700">
         WAITTER
-        <Text size={24}>
-          APP
-        </Text>
-
+        <Text size={24}>APP</Text>
       </Text>
     </Container>
   );
