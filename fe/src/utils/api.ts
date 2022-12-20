5d6aa87e040e8c3cce987fc6c947cc0c3d17f748
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2022-12-20 00:45:57 -0300

    upload dist frontend

--- fe/src/utils/api.ts
+++ fe/src/utils/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const api = axios.create({
-  baseURL: 'http://192.168.1.8:3001',
+  baseURL: process.env.API_URI,
 });
